58ecd135fee1592892734ede9903243ee4cce546
nirgodin <70273595+nirgodin@users.noreply.github.com>
2022-03-12 21:02:29 +0200

    - Finalized translation logic - Extracted function to utils

--- bio_enrichers/tools/wikipedia_manager.py
+++ bio_enrichers/tools/wikipedia_manager.py
@@ -1,28 +1,15 @@
-from functools import lru_cache
 from typing import Dict, Union
 
-from wikipediaapi import Wikipedia, WikipediaPage
-from google.cloud import translate_v2 as translate
+from wikipediaapi import WikipediaPage
 
-HEBREW_ABBR = 'he'
-ENGLISH_ABBR = 'en'
-
-
-@lru_cache(maxsize=1)
-def get_hebrew_wikipedia() -> Wikipedia:
-    return Wikipedia(HEBREW_ABBR)
-
-
-@lru_cache(maxsize=1)
-def get_english_wikipedia() -> Wikipedia:
-    return Wikipedia(ENGLISH_ABBR)
+from bio_enrichers.tools.wikipedia_manager_utils import HEBREW_ABBR, get_hebrew_wikipedia, get_english_wikipedia, \
+    has_any_hebrew_chars, translate_page_title
 
 
 class WikipediaManager:
     def __init__(self):
         self._he_wiki = get_hebrew_wikipedia()
         self._en_wiki = get_english_wikipedia()
-        self._translate_client = translate.Client()
 
     def get_page_summary(self, page_title: str) -> str:
         page = self._get_page(page_title)
@@ -32,38 +19,37 @@ class WikipediaManager:
         return page.summary
 
     def _get_page(self, page_title: str) -> Union[WikipediaPage, None]:
-        hebrew_page = self._get_hebrew_page_directly(page_title)
-        if hebrew_page:
-            return hebrew_page
+        if has_any_hebrew_chars(page_title):
+            return self._get_hebrew_page_directly(page_title)
 
-        english_page = self._en_wiki.page(page_title)
-        if not english_page.exists():
-            return None
-
-        translated_hebrew_page = self._get_hebrew_page_from_english_page(english_page)
-        if translated_hebrew_page:
-            return translated_hebrew_page
-
-        return english_page
+        return self._get_hebrew_page_from_english_page(page_title)
 
     def _get_hebrew_page_directly(self, page_title: str) -> WikipediaPage:
         hebrew_page = self._he_wiki.page(page_title)
         if hebrew_page.exists():
             return hebrew_page
 
-        translated_title = self._translate_page_title(page_title)
+    def _get_hebrew_page_from_english_page(self, page_title: str) -> Union[WikipediaPage, None]:
+        english_page = self._en_wiki.page(page_title)
+        hebrew_langlink_page = self._get_hebrew_page_using_langlinks(english_page)
+
+        if hebrew_langlink_page:
+            return hebrew_langlink_page
+
+        return self._get_hebrew_page_using_translation(page_title)
+
+    @staticmethod
+    def _get_hebrew_page_using_langlinks(english_page: WikipediaPage) -> Union[WikipediaPage, None]:
+        if not english_page.exists():
+            return None
+
+        available_translated_pages: Dict[str, WikipediaPage] = english_page.langlinks
+        return available_translated_pages.get(HEBREW_ABBR, None)
+
+    def _get_hebrew_page_using_translation(self, page_title: str):
+        translated_title = translate_page_title(page_title)
         if translated_title:
             hebrew_page_with_translated_title = self._he_wiki.page(translated_title)
 
             if hebrew_page_with_translated_title.exists():
                 return hebrew_page_with_translated_title
-
-    def _translate_page_title(self, page_title: str) -> str:
-        response: Dict[str, str] = self._translate_client.translate(page_title, target_language=HEBREW_ABBR)
-        if 'translatedText' in response.keys():
-            return response['translatedText']
-
-    @staticmethod
-    def _get_hebrew_page_from_english_page(english_page: WikipediaPage) -> WikipediaPage:
-        available_translated_pages: Dict[str, WikipediaPage] = english_page.langlinks
-        return available_translated_pages.get(HEBREW_ABBR, None)
