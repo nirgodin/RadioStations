f67cc9b1ff896e243a46b8891f5bee995cbddef4
nirgodin <70273595+nirgodin@users.noreply.github.com>
2022-12-09 16:15:40 +0200

    Removed redundant consts, imports and comments

--- data_collection/spotify.py
+++ data_collection/spotify.py
@@ -1,15 +1,9 @@
 from functools import lru_cache
-from typing import Union, List
+from typing import Union
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-
-DURATION = 'duration_ms'
-POPULARITY = 'popularity'
-GENRES = 'genres'
-ARTISTS = 'artists'
-EXTERNAL_URLS = 'external_urls'
-SPOTIFY = 'spotify'
 TRACKS = 'tracks'
 ITEMS = 'items'
 URI = 'uri'
@@ -21,22 +15,8 @@ def get_spotipy():
 
 
 class Spotify:
-
     def __init__(self):
         self.sp = get_spotipy()
-
-    # def get_duration(self):
-    #     return self._track[DURATION]
-    #
-    # def get_popularity(self) -> Union[int, None]:
-    #     return self._track[POPULARITY]
-    #
-    # def get_genres(self) -> Union[List[str], None]:
-    #     return self._artist[GENRES]
-    #
-    # def _get_artist(self):
-    #     artist = self._track[ARTISTS][0][EXTERNAL_URLS][SPOTIFY]
-    #     return self.sp.artist(artist)
 
     def _get_track(self, artist: str, track: str):
         track_id = self._get_track_id(artist, track)
