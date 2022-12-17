f50284843170f05daac9d1507932d3e30994d0b3
nirgodin <70273595+nirgodin@users.noreply.github.com>
2022-12-17 17:41:19 +0200

    Pass argument to decide which columns are going to be used by drop duplicates subset

--- data_processing/data_merger.py
+++ data_processing/data_merger.py
@@ -1,5 +1,5 @@
 import os
-from typing import Generator, Optional
+from typing import Generator, Optional, List
 
 import pandas as pd
 from pandas import DataFrame
@@ -10,10 +10,12 @@ from consts.data_consts import NAME, ADDED_AT, STATION, SCRAPED_AT
 
 class DataMerger:
     @staticmethod
-    def merge(dir_path: str, output_path: Optional[str] = None) -> DataFrame:
+    def merge(dir_path: str,
+              output_path: Optional[str] = None,
+              drop_duplicates_on: List[str] = (NAME, ADDED_AT, STATION)) -> DataFrame:
         files_data = DataMerger._generate_files_data(dir_path)
         merged_data: DataFrame = pd.concat(files_data)
-        non_duplicated_data = merged_data.drop_duplicates(subset=[NAME, ADDED_AT, STATION])
+        non_duplicated_data = merged_data.drop_duplicates(subset=drop_duplicates_on)
 
         if output_path is not None:
             non_duplicated_data.to_csv(output_path, index=False)
@@ -28,7 +30,8 @@ class DataMerger:
             for file_name in dir_files:
                 progress_bar.update(1)
 
-                yield DataMerger._generate_single_file_data(dir_path, file_name)
+                if DataMerger._is_csv_file(file_name):
+                    yield DataMerger._generate_single_file_data(dir_path, file_name)
 
     @staticmethod
     def _generate_single_file_data(dir_path: str, file_name: str) -> DataFrame:
@@ -37,3 +40,7 @@ class DataMerger:
         file_data[SCRAPED_AT] = file_name
 
         return file_data
+
+    @staticmethod
+    def _is_csv_file(file_name: str) -> bool:
+        return file_name.endswith('.csv')
