8057a86259a459601e799b7d35dbdf84cf1c304f
nirgodin <70273595+nirgodin@users.noreply.github.com>
2022-12-24 16:14:05 +0200

    Robust kan gimel analyzer

--- analysis/kan_gimel_analyzer.py
+++ analysis/kan_gimel_analyzer.py
@@ -4,21 +4,47 @@ from typing import List, Dict, Optional
 import pandas as pd
 from pandas import DataFrame
 
+from consts.data_consts import NAME, ARTIST_NAME, MAIN_ALBUM, STATION
 from consts.path_consts import KAN_GIMEL_ANALYZER_OUTPUT_PATH, MERGED_DATA_PATH
+from consts.playlists_consts import KAN_GIMEL
+
+COUNT = 'count'
+COUNT_THRESHOLD = 8
+EXCLUDED_ARTISTS = [
+    '2Pac',
+    'Dire Straits'
+]
 
 
 class KanGimelAnalyzer:
     def analyze(self, data_path: str, output_path: Optional[str] = None) -> Dict[str, List[str]]:
         data = pd.read_csv(data_path)
-        kan_gimel_data = data[data['station'] == 'kan_gimel']
+        kan_gimel_data = data[data[STATION] == KAN_GIMEL]
+        valid_artists_names = self._extract_valid_artists_names(kan_gimel_data)
+        valid_kan_gimel_data = kan_gimel_data[kan_gimel_data[ARTIST_NAME].isin(valid_artists_names)]
 
-        return self._build_output_json_data(kan_gimel_data, output_path)
+        return self._build_output_json_data(valid_kan_gimel_data, output_path)
+
+    def _extract_valid_artists_names(self, data: DataFrame) -> List[str]:
+        artists_count = self._get_artists_play_count(data)
+        valid_artists_data = artists_count[artists_count[COUNT] >= COUNT_THRESHOLD]
+        valid_artists_names = valid_artists_data[ARTIST_NAME].tolist()
+
+        return [artist for artist in valid_artists_names if artist not in EXCLUDED_ARTISTS]
+
+    @staticmethod
+    def _get_artists_play_count(data: DataFrame) -> DataFrame:
+        raw_count_data = data.groupby(by=ARTIST_NAME).count().reset_index(level=0)
+        count_data = raw_count_data[[ARTIST_NAME, NAME]]
+        count_data.columns = [ARTIST_NAME, COUNT]
+
+        return count_data
 
     def _build_output_json_data(self, data: DataFrame, output_path: Optional[str]) -> Dict[str, List[str]]:
         jsonified_data = {
-            'tracks': self._extract_unique_column_values(data, 'name'),
-            'artists': self._extract_unique_column_values(data, 'artist_name'),
-            'albums': self._extract_unique_column_values(data, 'main_album')
+            'tracks': self._extract_unique_column_values(data, NAME),
+            'artists': self._extract_unique_column_values(data, ARTIST_NAME),
+            'albums': self._extract_unique_column_values(data, MAIN_ALBUM)
         }
 
         if output_path is not None:
--- consts/playlists_consts.py
+++ consts/playlists_consts.py
@@ -1,9 +1,15 @@
+GLGLZ = 'glglz'
+KAN_88 = 'kan_88'
+ECO_99 = 'eco_99'
+KAN_GIMEL = 'kan_gimel'
+FM_103 = '103_fm'
+
 STATIONS = {
-    'glglz': '18cUFeM5Q75ViwevsMQM1j',
-    'kan_88': '6EOBRaHfU63LNFKLJmeS3S',
-    'eco_99': '07QpDB6YxM6flpbmdZTZNX',
-    'kan_gimel': '0JFp62JAJSri8WbxEYdFCY',
-    '103_fm': '2pBih0TFRCULBEOhKcFQ7Y',
+    GLGLZ: '18cUFeM5Q75ViwevsMQM1j',
+    KAN_88: '6EOBRaHfU63LNFKLJmeS3S',
+    ECO_99: '07QpDB6YxM6flpbmdZTZNX',
+    KAN_GIMEL: '0JFp62JAJSri8WbxEYdFCY',
+    FM_103: '2pBih0TFRCULBEOhKcFQ7Y',
     'spotify_top_50_weekly': '37i9dQZEVXbJ5J1TrbkAF9',
     'spotify_top_50_israel_daily': '37i9dQZEVXbJ6IpvItkve3',
     'spotify_top_50_global_daily': '37i9dQZEVXbMDoHDwVN2tF',
