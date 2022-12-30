caaa3023c4e9ff36e16eda8c6734ed4bf67face2
nirgodin <70273595+nirgodin@users.noreply.github.com>
2022-12-30 13:22:35 +0200

    Added mapping of musical keys numbers to names

--- consts/audio_features_consts.py
+++ consts/audio_features_consts.py
@@ -23,3 +23,18 @@ AUDIO_FEATURES = [
     TEMPO,
     VALENCE
 ]
+
+KEY_NAMES_MAPPING = {
+    0: 'C',
+    1: 'C#',
+    2: 'E',
+    3: 'D#',
+    4: 'E',
+    5: 'F',
+    6: 'F#',
+    7: 'G',
+    8: 'G#',
+    9: 'A',
+    10: 'A#',
+    11: 'B'
+}
--- data_processing/pre_processors/audio_features_pre_processor.py
+++ data_processing/pre_processors/audio_features_pre_processor.py
@@ -1,9 +1,21 @@
 from pandas import DataFrame
 
-from consts.data_consts import ARTIST_NAME, NAME, DURATION_MS, SCRAPED_AT
+from consts.audio_features_consts import KEY, KEY_NAMES_MAPPING
+from consts.data_consts import ARTIST_NAME, NAME, DURATION_MS, SCRAPED_AT, TYPE, TRACK_HREF, ANALYSIS_URL, \
+    TIME_SIGNATURE, ERROR
 from consts.path_consts import AUDIO_FEATURES_BASE_DIR, AUDIO_FEATURES_DATA_PATH
 from data_processing.data_merger import DataMerger
 from data_processing.pre_processors.pre_processor_interface import IPreProcessor
+
+AUDIO_FEATURES_DROP_COLUMNS = [
+    DURATION_MS,
+    SCRAPED_AT,
+    TYPE,
+    TRACK_HREF,
+    ANALYSIS_URL,
+    TIME_SIGNATURE,
+    ERROR
+]
 
 
 class AudioFeaturesPreProcessor(IPreProcessor):
@@ -14,6 +26,8 @@ class AudioFeaturesPreProcessor(IPreProcessor):
             output_path=AUDIO_FEATURES_DATA_PATH
         )
         audio_features.drop([DURATION_MS, SCRAPED_AT], axis=1, inplace=True)
+        audio_features[KEY] = audio_features[KEY].map(KEY_NAMES_MAPPING)
+
         return data.merge(
             right=audio_features,
             how='left',
