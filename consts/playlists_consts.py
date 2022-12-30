55a6c2f3a0696735e34096d3c90cd9a3ec35688c
nirgodin <70273595+nirgodin@users.noreply.github.com>
2022-12-30 14:21:16 +0200

    Extracted more consts

--- consts/playlists_consts.py
+++ consts/playlists_consts.py
@@ -3,6 +3,11 @@ KAN_88 = 'kan_88'
 ECO_99 = 'eco_99'
 KAN_GIMEL = 'kan_gimel'
 FM_103 = '103_fm'
+SPOTIFY_TOP_50_WEEKLY = 'spotify_top_50_weekly'
+SPOTIFY_TOP_50_ISRAEL_DAILY = 'spotify_top_50_israel_daily'
+SPOTIFY_TOP_50_GLOBAL_DAILY = 'spotify_top_50_global_daily'
+SPOTIFY_VIRAL_50_ISRAEL = 'spotify_viral_50_israel'
+
 
 STATIONS = {
     GLGLZ: '18cUFeM5Q75ViwevsMQM1j',
@@ -10,8 +15,8 @@ STATIONS = {
     ECO_99: '07QpDB6YxM6flpbmdZTZNX',
     KAN_GIMEL: '0JFp62JAJSri8WbxEYdFCY',
     FM_103: '2pBih0TFRCULBEOhKcFQ7Y',
-    'spotify_top_50_weekly': '37i9dQZEVXbJ5J1TrbkAF9',
-    'spotify_top_50_israel_daily': '37i9dQZEVXbJ6IpvItkve3',
-    'spotify_top_50_global_daily': '37i9dQZEVXbMDoHDwVN2tF',
-    'spotify_viral_50_israel': '37i9dQZEVXbNGlbFNNXxgC'
+    SPOTIFY_TOP_50_WEEKLY: '37i9dQZEVXbJ5J1TrbkAF9',
+    SPOTIFY_TOP_50_ISRAEL_DAILY: '37i9dQZEVXbJ6IpvItkve3',
+    SPOTIFY_TOP_50_GLOBAL_DAILY: '37i9dQZEVXbMDoHDwVN2tF',
+    SPOTIFY_VIRAL_50_ISRAEL: '37i9dQZEVXbNGlbFNNXxgC'
 }
