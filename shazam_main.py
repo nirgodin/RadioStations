304f568931d495f05da5a01e5a21f48830641b4d
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-02-03 14:14:17 +0200

    Added tracks about fetcher to shazam main

--- shazam_main.py
+++ shazam_main.py
@@ -6,6 +6,7 @@ from consts.path_consts import MERGED_DATA_PATH
 from data_collection.shazam.shazam_city_fetcher import ShazamCitiesFetcher
 from data_collection.shazam.shazam_country_fetcher import ShazamCountryFetcher
 from data_collection.shazam.shazam_search_fetcher import ShazamSearchFetcher
+from data_collection.shazam.shazam_song_about_fetcher import ShazamTrackAboutFetcher
 
 
 async def run():
@@ -24,6 +25,10 @@ async def run():
     print('Starting to fetch tracks ids')
     data = pd.read_csv(MERGED_DATA_PATH)
     await search_fetcher.search_tracks(data=data, max_tracks=100)
+
+    track_about_fetcher = ShazamTrackAboutFetcher()
+    print('Starting to fetch tracks about')
+    await track_about_fetcher.fetch_tracks_about(max_tracks=100)
 
 
 if __name__ == '__main__':
