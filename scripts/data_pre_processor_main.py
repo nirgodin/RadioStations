64475bf389cc1d24b82683e62538ee2b6eab3bb7
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-03-17 18:05:53 +0200

    Pass data_pre_processor_main.py to script die

--- scripts/data_pre_processor_main.py
+++ scripts/data_pre_processor_main.py
@@ -3,7 +3,5 @@ from data_processing.data_pre_processor import DataPreProcessor
 
 
 if __name__ == '__main__':
-    pre_processor = DataPreProcessor(
-        max_year=2023
-    )
+    pre_processor = DataPreProcessor(max_year=2023)
     pre_processor.pre_process(output_path=MERGED_DATA_PATH)
