4fdbbda17502e8f0dab637b8830bfd9ba982d7da
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-03-23 11:38:38 +0200

    Added progress bar to snapshots runner

--- data_collection/spotify/radio_stations_snapshots_runner.py
+++ data_collection/spotify/radio_stations_snapshots_runner.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
+from tqdm import tqdm
 
 from consts.miscellaneous_consts import UTF_8_ENCODING
 from consts.path_consts import RADIO_STATIONS_PLAYLIST_SNAPSHOT_PATH_FORMAT
@@ -21,8 +22,10 @@ class RadioStationsSnapshotsRunner:
     def _get_radio_stations_snapshots() -> DataFrame:
         dfs = []
 
-        for station_name, station_id in STATIONS.items():
-            station = Station(name=station_name, id=station_id)
-            dfs.append(station.to_dataframe())
+        with tqdm(total=len(STATIONS)) as progress_bar:
+            for station_name, station_id in STATIONS.items():
+                station = Station(name=station_name, id=station_id)
+                dfs.append(station.to_dataframe())
+                progress_bar.update(1)
 
         return pd.concat(dfs)
