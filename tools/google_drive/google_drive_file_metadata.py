0f15ddf9b7f9901b0b6eece4b4269473c0d380e3
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-04-06 18:26:08 +0300

    Replaced file name

--- tools/google_drive/google_drive_file_metadata.py
+++ tools/google_drive/google_drive_file_metadata.py
@@ -1,14 +1,23 @@
-import os
 from dataclasses import dataclass
+from typing import Optional
+
+from utils.datetime_utils import get_current_datetime
 
 
 @dataclass
 class GoogleDriveFileMetadata:
     local_path: str
     drive_folder_id: str
+    file_name: Optional[str] = None
 
     def __post_init__(self):
         self.metadata = {
-            'name': os.path.basename(self.local_path),
+            'name': self._get_file_name(),
             'parents': [self.drive_folder_id]
         }
+
+    def _get_file_name(self) -> str:
+        if self.file_name is not None:
+            return self.file_name
+
+        return get_current_datetime()
