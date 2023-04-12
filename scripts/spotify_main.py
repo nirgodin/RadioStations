c4de52b61dc9e99ff68a98ecd9a8d500db205531
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-04-12 22:15:55 +0300

    Temporarily execute weekly runs only if not remote

--- scripts/spotify_main.py
+++ scripts/spotify_main.py
@@ -7,37 +7,39 @@ from data_collection.spotify.weekly_run.spotify_collector_config import SpotifyC
 from data_collection.spotify.weekly_run.spotify_weekly_runner import SpotifyWeeklyRunner
 from data_collection.spotify.radio_stations_snapshots_runner import RadioStationsSnapshotsRunner
 from data_collection.spotify.weekly_run.spotify_weekly_runner_config import SpotifyWeeklyRunnerConfig
+from utils.general_utils import is_remote_run
 
 
 async def run() -> None:
     radio_stations_snapshots_runner = RadioStationsSnapshotsRunner()
     radio_stations_snapshots_runner.run()
 
-    spotify_weekly_run_config = SpotifyWeeklyRunnerConfig(
-        artists_ids=SpotifyCollectorConfig(
-            name='artists ids collector',
-            weekday=2,
-            collector=ArtistsIDsCollector,
-            chunk_size=100,
-            max_chunks_number=10
-        ),
-        albums_details=SpotifyCollectorConfig(
-            name='albums details collector',
-            weekday=3,
-            collector=AlbumsDetailsCollector,
-            chunk_size=50,
-            max_chunks_number=10
-        ),
-        audio_features=SpotifyCollectorConfig(
-            name='audio features collector',
-            weekday=4,
-            collector=AudioFeaturesCollector,
-            chunk_size=1000,
-            max_chunks_number=5
+    if not is_remote_run():
+        spotify_weekly_run_config = SpotifyWeeklyRunnerConfig(
+            artists_ids=SpotifyCollectorConfig(
+                name='artists ids collector',
+                weekday=2,
+                collector=ArtistsIDsCollector,
+                chunk_size=100,
+                max_chunks_number=10
+            ),
+            albums_details=SpotifyCollectorConfig(
+                name='albums details collector',
+                weekday=3,
+                collector=AlbumsDetailsCollector,
+                chunk_size=50,
+                max_chunks_number=10
+            ),
+            audio_features=SpotifyCollectorConfig(
+                name='audio features collector',
+                weekday=4,
+                collector=AudioFeaturesCollector,
+                chunk_size=1000,
+                max_chunks_number=5
+            )
         )
-    )
-    weekly_runner = SpotifyWeeklyRunner(spotify_weekly_run_config)
-    await weekly_runner.run()
+        weekly_runner = SpotifyWeeklyRunner(spotify_weekly_run_config)
+        await weekly_runner.run()
 
 
 if __name__ == '__main__':
