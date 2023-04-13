6c8a4b281421cdfabe5d37b8117240e98e8750a3
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-04-13 11:59:55 +0300

    Added dvc add and push function

--- utils/vcs_utils.py
+++ utils/vcs_utils.py
@@ -1,7 +1,10 @@
 import os
+import subprocess
 from typing import List
 
 from git import Repo
+
+from consts.path_consts import MAIN_DATA_DIR_PATH, DATA_DVC_PATH
 
 
 def commit_and_push(files_paths: List[str], commit_message: str, branch_name: str = 'main') -> None:
@@ -13,3 +16,13 @@ def commit_and_push(files_paths: List[str], commit_message: str, branch_name: st
     repo.git.merge('--no-ff', '-m', merge_message, branch_name)
     origin = repo.remote()
     origin.push()
+
+
+def dvc_add_and_push(commit_message: str, branch_name: str = 'main') -> None:
+    subprocess.call(['dvc', 'add', MAIN_DATA_DIR_PATH])
+    commit_and_push(
+        files_paths=[DATA_DVC_PATH],
+        commit_message=commit_message,
+        branch_name=branch_name
+    )
+    subprocess.call(['dvc', 'push'])
