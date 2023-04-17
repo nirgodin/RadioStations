bfa16817a8c2414f17cd7c96c451b22cf0535bfb
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-04-17 21:09:04 +0300

    Initialized gender_pre_processor.py

--- data_processing/pre_processors/gender_pre_processor.py
+++ data_processing/pre_processors/gender_pre_processor.py
@@ -1,11 +1,21 @@
+import pandas as pd
 from pandas import DataFrame
 
+from consts.data_consts import ARTIST_NAME
+from consts.path_consts import MAPPED_GENDERS_OUTPUT_PATH, MERGED_DATA_PATH
 from data_processing.pre_processors.pre_processor_interface import IPreProcessor
 
 
 class GenderPreProcessor(IPreProcessor):
     def pre_process(self, data: DataFrame) -> DataFrame:
-        return data
+        genders_mapping = pd.read_csv(MAPPED_GENDERS_OUTPUT_PATH)
+        merged_data = data.merge(
+            right=genders_mapping,
+            how='left',
+            on=ARTIST_NAME
+        )
+
+        return merged_data
 
     @property
     def name(self) -> str:
