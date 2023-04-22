0d0ec6aaf3e43b2ee06d50870cc83c27bb338152
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-04-22 19:19:21 +0300

    Working version

--- utils/spotify_utils.py
+++ utils/spotify_utils.py
@@ -1,9 +1,11 @@
+import os
 from functools import lru_cache
-from typing import Dict
+from typing import Dict, Optional
 
 import spotipy
 from spotipy import SpotifyClientCredentials
 
+from consts.env_consts import SPOTIPY_PORT
 from data_collection.spotify.access_token_generator import AccessTokenGenerator
 
 
@@ -20,3 +22,18 @@ def build_spotify_headers() -> Dict[str, str]:
 @lru_cache(maxsize=1)
 def get_spotipy():
     return spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+
+@lru_cache(maxsize=1)
+def get_port() -> int:
+    return int(os.environ[SPOTIPY_PORT])
+
+
+@lru_cache(maxsize=1)
+def build_spotify_authorization_headers() -> Dict[str, str]:
+    bearer_token = AccessTokenGenerator.generate()
+    return {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {bearer_token}"
+    }
