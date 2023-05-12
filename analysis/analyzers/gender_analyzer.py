8cd84911105fa774ec633ad2b0b7f65b91401f2a
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-05-12 16:42:44 +0300

    Integrate equal playlists data to gender analyzer

--- analysis/analyzers/gender_analyzer.py
+++ analysis/analyzers/gender_analyzer.py
@@ -5,11 +5,11 @@ from tqdm import tqdm
 
 from analysis.analyzer_interface import IAnalyzer
 from consts.data_consts import ARTIST_NAME
-from consts.gender_consts import SOURCE, ISRAELI_WIKIPEDIA, OPENAI, GENERAL_WIKIPEDIA
+from consts.gender_consts import SOURCE, ISRAELI_WIKIPEDIA, OPENAI, GENERAL_WIKIPEDIA, SPOTIFY_EQUAL_PLAYLISTS
 from consts.miscellaneous_consts import UTF_8_ENCODING
 from consts.openai_consts import ARTIST_GENDER
 from consts.path_consts import WIKIPEDIA_ISRAELI_ARTISTS_GENDER_PATH, OPENAI_GENDERS_PATH, \
-    WIKIPEDIA_OPENAI_UNKNOWN_GENDERS_PATH, MAPPED_GENDERS_OUTPUT_PATH
+    WIKIPEDIA_OPENAI_UNKNOWN_GENDERS_PATH, MAPPED_GENDERS_OUTPUT_PATH, SPOTIFY_EQUAL_PLAYLISTS_OUTPUT_PATH
 from data_collection.wikipedia.gender.genders import Genders
 from utils.data_utils import map_df_columns, groupby_artists_by_desc_popularity
 
@@ -58,6 +58,7 @@ class GenderAnalyzer(IAnalyzer):
     @property
     def _sources(self) -> Dict[str, Dict[str, str]]:
         return {
+            SPOTIFY_EQUAL_PLAYLISTS: self._read_and_map_df_columns(SPOTIFY_EQUAL_PLAYLISTS_OUTPUT_PATH),
             ISRAELI_WIKIPEDIA: self._read_and_map_df_columns(WIKIPEDIA_ISRAELI_ARTISTS_GENDER_PATH),
             OPENAI: self._read_and_map_df_columns(OPENAI_GENDERS_PATH),
             GENERAL_WIKIPEDIA: self._read_and_map_df_columns(WIKIPEDIA_OPENAI_UNKNOWN_GENDERS_PATH)
