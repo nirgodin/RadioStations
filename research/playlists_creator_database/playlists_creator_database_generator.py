0145ecec5b82312014fca6ff02ddef1c83ca43a1
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-05-27 16:22:08 +0300

    Added google drive upload of playlists creator database generator

--- research/playlists_creator_database/playlists_creator_database_generator.py
+++ research/playlists_creator_database/playlists_creator_database_generator.py
@@ -7,9 +7,12 @@ from sklearn.preprocessing import FunctionTransformer
 
 from consts.aggregation_consts import FIRST, MEDIAN, COUNT
 from consts.data_consts import SONG, URI, DURATION_MINUTES, DURATION_MS, MAJOR, MINOR
+from consts.env_consts import PLAYLISTS_CREATOR_DATABASE_DRIVE_ID
 from consts.path_consts import MERGED_DATA_PATH, PLAYLISTS_CREATOR_DATABASE_OUTPUT_PATH
 from research.playlists_creator_database.playlists_creator_database_consts import DROPPABLE_COLUMNS, \
     GROUPBY_FIRST_COLUMNS, GROUPBY_MEDIAN_COLUMNS, LINEAR_TRANSFORMED_COLUMNS
+from tools.google_drive.google_drive_upload_metadata import GoogleDriveUploadMetadata
+from utils.drive_utils import upload_files_to_drive
 from utils.general_utils import chain_dicts
 
 ISRAELI_RADIO_PLAY_COUNT = 'israeli_radio_play_count'
@@ -24,8 +27,6 @@ class PlaylistsCreatorDatabaseGenerator:
         pre_processed_data = self._apply_transformations(groubyed_data)
         pre_processed_data[DURATION_MINUTES] = pre_processed_data[DURATION_MS].apply(lambda x: x / (1000 * 60))
         pre_processed_data.drop(DURATION_MS, axis=1, inplace=True)
-
-        pre_processed_data.to_csv(PLAYLISTS_CREATOR_DATABASE_OUTPUT_PATH, index=False)
 
     def _groupby_data(self, data: DataFrame) -> DataFrame:
         relevant_data = data.drop(DROPPABLE_COLUMNS, axis=1)
@@ -68,6 +69,16 @@ class PlaylistsCreatorDatabaseGenerator:
     @staticmethod
     def _map_mode(data: DataFrame) -> DataFrame:
         return data.applymap(lambda x: MAJOR if x == 1 else MINOR)
+
+    @staticmethod
+    def _output_results(pre_processed_data: DataFrame) -> None:
+        pre_processed_data.to_csv(PLAYLISTS_CREATOR_DATABASE_OUTPUT_PATH, index=False)
+        upload_metadata = GoogleDriveUploadMetadata(
+            local_path=PLAYLISTS_CREATOR_DATABASE_OUTPUT_PATH,
+            drive_folder_id=PLAYLISTS_CREATOR_DATABASE_DRIVE_ID
+        )
+
+        upload_files_to_drive(upload_metadata)
 
 
 if __name__ == '__main__':
