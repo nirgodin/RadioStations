a87bcbe9c7ca7121bab263c9397e18b732af3feb
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-06-02 12:45:36 +0300

    Integrated kwargs in spotify weekly run and added israeli playlists collector

--- data_collection/spotify/collectors/playlists_artists_collector.py
+++ data_collection/spotify/collectors/playlists_artists_collector.py
@@ -8,53 +8,64 @@ from pandas import DataFrame
 from tqdm import tqdm
 
 from consts.data_consts import TRACK, ARTISTS, ARTIST_ID, ID, ARTIST_NAME, NAME, STATION
-from consts.openai_consts import ARTIST_GENDER
-from consts.path_consts import SPOTIFY_EQUAL_PLAYLISTS_OUTPUT_PATH
-from consts.playlists_consts import EQUAL_PLAYLISTS
+from consts.miscellaneous_consts import NAMED_PLAYLISTS, OUTPUT_PATH, RECORD_KEY, RECORD_VALUE
 from data_collection.spotify.base_spotify_collector import BaseSpotifyCollector
 from data_collection.spotify.collectors.playlists_collector import PlaylistsCollector
 from data_collection.spotify.collectors.radio_stations_snapshots.data_classes.playlist import Playlist
-from data_collection.wikipedia.gender.genders import Genders
 from tools.environment_manager import EnvironmentManager
 from utils.file_utils import append_to_csv
 from utils.general_utils import chain_lists, binary_search
 from utils.spotify_utils import build_spotify_headers
 
 
-class EqualPlaylistsCollector(BaseSpotifyCollector):
+class PlaylistsArtistsCollector(BaseSpotifyCollector):
     def __init__(self, session: ClientSession, chunk_size: int, max_chunks_number: int):
         super().__init__(session, chunk_size, max_chunks_number)
         self._session = session
         self._playlists_collector = PlaylistsCollector(self._session)
 
-    async def collect(self) -> None:
+    async def collect(self, **kwargs) -> None:
         EnvironmentManager().set_env_variables()
-        playlists = await self._playlists_collector.collect(EQUAL_PLAYLISTS)
-        artists = self._generate_playlists_main_artists(playlists)
+        playlists = await self._playlists_collector.collect(kwargs[NAMED_PLAYLISTS])
+        artists = self._generate_playlists_main_artists(
+            playlists=playlists,
+            record_key=kwargs[RECORD_KEY],
+            record_value=kwargs[RECORD_VALUE]
+        )
         records = chain_lists(artists)
-        data = self._add_non_existing_artists(records)
+        output_path = kwargs[OUTPUT_PATH]
+        data = self._add_non_existing_artists(records, output_path)
 
-        append_to_csv(data, SPOTIFY_EQUAL_PLAYLISTS_OUTPUT_PATH)
+        append_to_csv(data, output_path)
 
-    def _generate_playlists_main_artists(self, playlists: List[Playlist]) -> Generator[List[Dict[str, str]], None, None]:
+    def _generate_playlists_main_artists(self,
+                                         playlists: List[Playlist],
+                                         record_key: str,
+                                         record_value: str) -> Generator[List[Dict[str, str]], None, None]:
         for playlist in playlists:
             print(f'Starting to extract `{playlist.station}` artists')
 
-            yield self._extract_single_playlist_main_artists(playlist)
+            yield self._extract_single_playlist_main_artists(playlist, record_key, record_value)
 
-    def _extract_single_playlist_main_artists(self, playlist: Playlist) -> List[Dict[str, str]]:
+    def _extract_single_playlist_main_artists(self,
+                                              playlist: Playlist,
+                                              record_key: str,
+                                              record_value: str) -> List[Dict[str, str]]:
         records = []
 
         with tqdm(total=len(playlist.tracks)) as progress_bar:
             for track in playlist.tracks:
-                record = self._extract_single_track_main_artist(track, playlist.station)
+                record = self._extract_single_track_main_artist(track, playlist.station, record_key, record_value)
                 records.append(record)
                 progress_bar.update(1)
 
         return records
 
     @staticmethod
-    def _extract_single_track_main_artist(track: dict, playlist_name: str) -> Optional[Dict[str, str]]:
+    def _extract_single_track_main_artist(track: dict,
+                                          playlist_name: str,
+                                          record_key: str,
+                                          record_value: str) -> Optional[Dict[str, str]]:
         artists = track.get(TRACK, {}).get(ARTISTS, [])
         if not artists:
             return
@@ -63,13 +74,13 @@ class EqualPlaylistsCollector(BaseSpotifyCollector):
         return {
             ARTIST_ID: main_artist[ID],
             ARTIST_NAME: main_artist[NAME],
-            ARTIST_GENDER: Genders.FEMALE.value,
+            record_key: record_value,
             STATION: playlist_name
         }
 
-    def _add_non_existing_artists(self, new_records: List[Dict[str, str]]) -> DataFrame:
+    def _add_non_existing_artists(self, new_records: List[Dict[str, str]], output_path: str) -> DataFrame:
         print('Starting to add non existing artists')
-        records = self._get_existing_artists_data()
+        records = self._get_existing_artists_data(output_path)
         existing_artists = [record[ARTIST_NAME] for record in records]
 
         with tqdm(total=len(records)) as progress_bar:
@@ -80,11 +91,11 @@ class EqualPlaylistsCollector(BaseSpotifyCollector):
         return pd.DataFrame.from_records(records)
 
     @staticmethod
-    def _get_existing_artists_data() -> List[Dict[str, str]]:
-        if not os.path.exists(SPOTIFY_EQUAL_PLAYLISTS_OUTPUT_PATH):
+    def _get_existing_artists_data(output_path: str) -> List[Dict[str, str]]:
+        if not os.path.exists(output_path):
             return []
 
-        data = pd.read_csv(SPOTIFY_EQUAL_PLAYLISTS_OUTPUT_PATH)
+        data = pd.read_csv(output_path)
         data.dropna(subset=[ARTIST_NAME], inplace=True)
 
         return data.to_dict(orient='records')
@@ -105,4 +116,5 @@ class EqualPlaylistsCollector(BaseSpotifyCollector):
 if __name__ == '__main__':
     client_session = ClientSession(headers=build_spotify_headers())
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(EqualPlaylistsCollector(client_session).collect())
+    collector = PlaylistsArtistsCollector(client_session, 5, 5)
+    loop.run_until_complete(collector.collect())
