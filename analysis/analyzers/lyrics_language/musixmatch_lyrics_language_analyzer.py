5d8cffca1fba91242ff768a303966e311cd87025
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-06-20 20:34:40 +0300

    Improved musixmatch language analyzer and added run of tracks languages analyzer on each run of script

--- analysis/analyzers/lyrics_language/musixmatch_lyrics_language_analyzer.py
+++ analysis/analyzers/lyrics_language/musixmatch_lyrics_language_analyzer.py
@@ -1,59 +1,69 @@
 import os
 from typing import Dict, Union, List, Optional
 
+import numpy as np
 import pandas as pd
-from pandas import DataFrame
 from tqdm import tqdm
 
 from analysis.analyzer_interface import IAnalyzer
 from consts.data_consts import ID
+from consts.language_consts import SCORE, LANGUAGE
 from consts.musixmatch_consts import LYRICS_BODY
 from consts.path_consts import MUSIXMATCH_TRACKS_LYRICS_PATH, MUSIXMATCH_TRACKS_LANGUAGES_PATH
+from tools.data_chunks_generator import DataChunksGenerator
 from tools.language_detector import LanguageDetector
-from utils.file_utils import read_json
+from utils.file_utils import read_json, append_to_csv
 
 
 class MusixmatchLyricsLanguageAnalyzer(IAnalyzer):
-    def __init__(self):
+    def __init__(self, chunk_size: int = 50, chunks_limit: Optional[int] = None):
+        self._chunk_size = chunk_size
+        self._chunks_limit = chunks_limit
         self._tracks_lyrics = read_json(MUSIXMATCH_TRACKS_LYRICS_PATH)
         self._language_detector = LanguageDetector()
+        self._data_chunks_generator = DataChunksGenerator(self._chunk_size)
 
     def analyze(self) -> None:
-        tracks_languages_records = self._extract_tracks_languages()
-        tracks_languages_data = pd.DataFrame.from_records(tracks_languages_records)
+        chunks = self._data_chunks_generator.generate_data_chunks(
+            lst=list(self._tracks_lyrics.keys()),
+            filtering_list=self._get_existing_tracks_ids()
+        )
 
-        tracks_languages_data.to_csv(MUSIXMATCH_TRACKS_LANGUAGES_PATH, index=False)
+        for i, chunk in enumerate(chunks):
+            if i == self._chunks_limit:
+                break
+            else:
+                self._extract_single_chunk_tracks_languages(chunk)
 
-    def _extract_tracks_languages(self) -> List[Dict[str, Union[str, float]]]:
+    def _extract_single_chunk_tracks_languages(self, chunk: List[str]):
+        language_records = self._get_tracks_language_records(chunk)
+        language_data = pd.DataFrame.from_records(language_records)
+        language_data = language_data
+
+        append_to_csv(data=language_data, output_path=MUSIXMATCH_TRACKS_LANGUAGES_PATH)
+
+    def _get_tracks_language_records(self, chunk: List[str]) -> List[Dict[str, Union[str, float]]]:
         records = []
-        spotify_tracks_ids = self._tracks_lyrics.keys()
 
-        with tqdm(total=len(spotify_tracks_ids)) as progress_bar:
-            for spotify_id in spotify_tracks_ids:
-                record = self._extract_single_track_language(spotify_id, progress_bar)
+        with tqdm(total=len(chunk)) as progress_bar:
+            for spotify_id in chunk:
+                progress_bar.update(1)
+                record = self._create_track_language_record(spotify_id)
 
                 if record is not None:
                     records.append(record)
 
         return records
 
-    def _extract_single_track_language(self, spotify_id: str, pbar: tqdm) -> Optional[Dict[str, Union[str, float]]]:
-        pbar.update(1)
-
-        if self._is_relevant_spotify_id(spotify_id):
-            return self._create_track_language_record(spotify_id)
-
-    def _is_relevant_spotify_id(self, spotify_id: str) -> bool:
-        if self._tracks_languages.empty:
-            return True
-
-        return spotify_id in self._tracks_languages[ID]
-
     def _create_track_language_record(self, spotify_id: str) -> Dict[str, Union[str, float]]:
         track_lyrics = self._extract_track_lyrics(spotify_id)
 
-        if track_lyrics is None:
-            return {ID: spotify_id}
+        if track_lyrics == "":
+            return {
+                LANGUAGE: np.nan,
+                SCORE: np.nan,
+                ID: spotify_id
+            }
 
         language_and_confidence = self._language_detector.detect_language(track_lyrics)
         language_and_confidence[ID] = spotify_id
@@ -61,14 +71,15 @@ class MusixmatchLyricsLanguageAnalyzer(IAnalyzer):
         return language_and_confidence
 
     def _extract_track_lyrics(self, spotify_id: str) -> Optional[str]:
-        return self._tracks_lyrics.get(spotify_id, {}).get(LYRICS_BODY, None)
+        return self._tracks_lyrics.get(spotify_id, {}).get(LYRICS_BODY, "")
 
-    @property
-    def _tracks_languages(self) -> DataFrame:
+    @staticmethod
+    def _get_existing_tracks_ids() -> List[str]:
         if not os.path.exists(MUSIXMATCH_TRACKS_LANGUAGES_PATH):
-            return pd.DataFrame()
+            return []
 
-        return pd.read_csv(MUSIXMATCH_TRACKS_LANGUAGES_PATH)
+        existing_data = pd.read_csv(MUSIXMATCH_TRACKS_LANGUAGES_PATH)
+        return existing_data[ID].tolist()
 
     @property
     def name(self) -> str:
--- scripts/musixmatch_main.py
+++ scripts/musixmatch_main.py
@@ -2,6 +2,7 @@ import asyncio
 
 import pandas as pd
 
+from analysis.analyzers.lyrics_language.musixmatch_lyrics_language_analyzer import MusixmatchLyricsLanguageAnalyzer
 from consts.path_consts import MERGED_DATA_PATH
 from data_collection.musixmatch.musixmatch_lyrics_fetcher import MusixmatchLyricsFetcher
 from data_collection.musixmatch.musixmatch_search_fetcher import MusixmatchSearchFetcher
@@ -15,11 +16,14 @@ async def run(request_limit: int):
     print(f'Starting to fetch {request_limit} tracks ids using MusixmatchSearchFetcher')
     await search_fetcher.fetch_tracks_ids(data=pd.read_csv(MERGED_DATA_PATH))
 
-    lyrics_fetcher = MusixmatchLyricsFetcher(request_limit=request_limit)
+    lyrics_fetcher = MusixmatchLyricsFetcher(request_limit=request_limit - 300)
     print(f'Starting to fetch {request_limit} tracks lyrics using MusixmatchLyricsFetcher')
     await lyrics_fetcher.fetch_tracks_lyrics()
 
+    language_analyzer = MusixmatchLyricsLanguageAnalyzer()
+    print(f'Starting to analyze Musixmatch tracks lyrics language')
+    language_analyzer.analyze()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(request_limit=990))
+    loop.run_until_complete(run(request_limit=1000))
