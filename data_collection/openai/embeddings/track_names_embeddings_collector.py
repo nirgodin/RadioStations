7924256a3ed9fffc751b668e9656b2ec3dc1e963
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-08-05 17:07:31 +0300

    F

--- data_collection/openai/embeddings/track_names_embeddings_collector.py
+++ data_collection/openai/embeddings/track_names_embeddings_collector.py
@@ -1,8 +1,9 @@
-from typing import Tuple, Generator, Optional
+from typing import Tuple, Generator, Optional, List
 
+import pandas as pd
 from tqdm import tqdm
 
-from consts.data_consts import NAME, ID
+from consts.data_consts import NAME, ID, URI
 from consts.path_consts import MERGED_DATA_PATH, \
     TRACK_NAMES_EMBEDDINGS_PATH
 from data_collection.openai.embeddings.base_embeddings_collector import BaseEmbeddingsCollector
@@ -18,13 +19,24 @@ class TrackNamesEmbeddingsCollector(BaseEmbeddingsCollector):
 
     def _generate_chunks(self) -> Generator[list, None, None]:
         return self._data_chunks_generator.generate_data_chunks(
-            lst=extract_column_existing_values(MERGED_DATA_PATH, [ID, NAME]),
-            filtering_list=extract_column_existing_values(TRACK_NAMES_EMBEDDINGS_PATH, [ID, NAME])
+            lst=self._get_track_names_for_unique_ids(),
+            filtering_list=extract_column_existing_values(TRACK_NAMES_EMBEDDINGS_PATH, [ID, NAME, URI])
         )
 
-    async def _extract_single_embeddings(self, progress_bar: tqdm, track_id_and_name: Tuple[str, str]) -> dict:
-        track_id, track_name = track_id_and_name
-        record = {NAME: track_name, ID: track_id}
+    @staticmethod
+    def _get_track_names_for_unique_ids() -> List[Tuple[str, str, str]]:
+        data = pd.read_csv(MERGED_DATA_PATH)
+        data.drop_duplicates(subset=[ID], inplace=True)
+
+        return list(data[[ID, NAME, URI]].itertuples(index=False, name=None))
+
+    async def _extract_single_embeddings(self, progress_bar: tqdm, track_id_name_and_uri: Tuple[str, str, str]) -> dict:
+        track_id, track_name, track_uri = track_id_name_and_uri
+        record = {
+            NAME: track_name,
+            ID: track_id,
+            URI: track_uri
+        }
         embeddings_record = await self._get_embeddings_record(track_name)
         record.update(embeddings_record)
         progress_bar.update(1)
