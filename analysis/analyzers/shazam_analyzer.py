f1c4952b27c8ab630eeb0fa4d2d4056743373fad
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-09-02 18:25:11 +0300

    Added song column

--- analysis/analyzers/shazam_analyzer.py
+++ analysis/analyzers/shazam_analyzer.py
@@ -1,6 +1,6 @@
 from analysis.analyzer_interface import IAnalyzer
 from consts.audio_features_consts import KEY
-from consts.data_consts import SCRAPED_AT, NAME, ARTIST_NAME
+from consts.data_consts import SCRAPED_AT, NAME, ARTIST_NAME, SONG
 from consts.media_forest_consts import RANK
 from consts.path_consts import SHAZAM_ISRAEL_DIR_PATH, SHAZAM_ISRAEL_MERGED_DATA
 from consts.shazam_consts import SHAZAM_RANK, TITLE, SUBTITLE
@@ -23,8 +23,13 @@ class ShazamAnalyzer(IAnalyzer):
     def analyze(self) -> None:
         data = self._data_merger.merge(SHAZAM_ISRAEL_DIR_PATH)
         data.rename(columns=SHAZAM_COLUMNS_MAPPING, inplace=True)
+        data[SONG] = data[[ARTIST_NAME, NAME]].apply(lambda x: self._to_song(*x), axis=1)
 
         to_csv(data, SHAZAM_ISRAEL_MERGED_DATA)
+
+    @staticmethod
+    def _to_song(artist_name: str, name: str) -> str:
+        return f'{artist_name} - {name}'
 
     @property
     def name(self) -> str:
