ae004d683b97097e8026f86ba9db3336c2b1c21c
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-09-25 13:39:56 +0300

    Removed cache maxsize

--- component_factory.py
+++ component_factory.py
@@ -7,11 +7,11 @@ from tools.language_detector import LanguageDetector
 
 class ComponentFactory:
     @staticmethod
-    @lru_cache(maxsize=1)
+    @lru_cache
     def get_language_detector() -> LanguageDetector:
         return LanguageDetector()
 
     @staticmethod
-    @lru_cache(maxsize=2)
+    @lru_cache
     def get_wikipedia(language_abbreviation: str) -> Wikipedia:
         return Wikipedia(language_abbreviation)
