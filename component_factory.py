eec09241d08553e23b396a4332f2e8a2ac216fe9
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-10-09 19:10:51 +0300

    Added postgres foundations

--- component_factory.py
+++ component_factory.py
@@ -1,7 +1,10 @@
+import os
 from functools import lru_cache
 
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from wikipediaapi import Wikipedia
 
+from consts.env_consts import DATABASE_URL
 from tools.language_detector import LanguageDetector
 
 
@@ -15,3 +18,11 @@ class ComponentFactory:
     @lru_cache
     def get_wikipedia(language_abbreviation: str) -> Wikipedia:
         return Wikipedia(language_abbreviation)
+
+    @staticmethod
+    @lru_cache
+    def get_database_engine() -> AsyncEngine:
+        url = os.environ[DATABASE_URL]
+        connect_args = {}  # {"ssl": "require"}  # TODO: Add back ssl when real database is utilized
+
+        return create_async_engine(url=url, connect_args=connect_args)
