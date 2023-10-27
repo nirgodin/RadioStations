058dbd0b80d95cd6bb1f41373c4f5d380c1f8075
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-10-27 17:32:10 +0300

    Fix track lyrics items pre processing

--- database/orm_models/track_lyrics.py
+++ database/orm_models/track_lyrics.py
@@ -1,5 +1,9 @@
+import json
+
 from sqlalchemy import Column, String, ForeignKey, ARRAY, SmallInteger, JSON, Enum, Float
 
+from consts.lyrics_consts import LYRICS_SOURCE, NUMBER_OF_WORDS, WORDS_COUNT
+from consts.musixmatch_consts import LYRICS
 from database.orm_models.base_orm_model import BaseORMModel
 from models.data_source import DataSource
 
@@ -11,10 +15,19 @@ class TrackLyrics(BaseORMModel):
     lyrics = Column(ARRAY(String))
     lyrics_source = Column(Enum(DataSource))
     language = Column(String)
-    language_confidence = Column(Float)
+    language_confidence = Column(SmallInteger)
     number_of_words = Column(SmallInteger)
     words_count = Column(JSON)
 
     @staticmethod
     def _pre_process_record_items(record_items: dict) -> dict:
-        pass
+        lyrics_source = record_items[LYRICS_SOURCE]
+        record_items[LYRICS_SOURCE] = None if lyrics_source is None else DataSource(lyrics_source)
+        language_score = record_items["score"]
+        record_items["language_confidence"] = None if language_score is None else round(language_score * 100)
+        number_of_words = record_items[NUMBER_OF_WORDS]
+        record_items[NUMBER_OF_WORDS] = None if number_of_words is None else int(number_of_words)
+        record_items[LYRICS] = None if record_items[LYRICS] is None else eval(record_items[LYRICS])
+        record_items[WORDS_COUNT] = None if record_items[WORDS_COUNT] is None else json.loads(record_items[WORDS_COUNT])
+
+        return record_items
