1f8af6576b3979b8663b5a7c6d3bf1c1c7dc601e
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-11-07 10:09:21 +0200

    Created working version for database insertions

--- data_collection_v2/snapshots_collector.py
+++ data_collection_v2/snapshots_collector.py
@@ -1,30 +1,19 @@
 import asyncio
-import os
 from typing import List
 
-import pandas as pd
 from aiohttp import ClientSession
-from pandas import DataFrame
 from postgres_client.postgres_operations import get_database_engine
 from spotipyio.logic.spotify_client import SpotifyClient
 
-from consts.data_consts import TRACK, ID, TRACKS, ITEMS
-from consts.env_consts import RADIO_STATIONS_SNAPSHOTS_DRIVE_ID
-from consts.path_consts import RADIO_STATIONS_PLAYLIST_SNAPSHOT_PATH_FORMAT
-from consts.playlists_consts import GLGLZ
-from data_collection.spotify.collectors.playlists_collector import PlaylistsCollector
-from data_collection.spotify.collectors.radio_stations_snapshots.data_classes.artist import Artist
-from data_collection.spotify.collectors.radio_stations_snapshots.data_classes.playlist import Playlist
+from consts.data_consts import ID, TRACKS, ITEMS
 from data_collection.spotify.collectors.radio_stations_snapshots.data_classes.station import Station
-from data_collection.spotify.collectors.radio_stations_snapshots.data_classes.track import Track
-from data_collection_v2.albums_database_inserter import AlbumsDatabaseInserter
-from data_collection_v2.artists_database_inserter import ArtistsDatabaseInserter
+from data_collection_v2.database_insertion.albums_database_inserter import AlbumsDatabaseInserter
+from data_collection_v2.database_insertion.artists_database_inserter import ArtistsDatabaseInserter
+from data_collection_v2.database_insertion.audio_features_database_inserter import AudioFeaturesDatabaseInserter
+from data_collection_v2.database_insertion.base_database_inserter import BaseDatabaseInserter
+from data_collection_v2.database_insertion.tracks_database_inserter import TracksDatabaseInserter
 from tools.environment_manager import EnvironmentManager
-from tools.google_drive.google_drive_upload_metadata import GoogleDriveUploadMetadata
 from tools.logging import logger
-from utils.datetime_utils import get_current_datetime
-from utils.drive_utils import upload_files_to_drive
-from utils.file_utils import to_csv
 from utils.spotify_utils import build_spotify_headers
 
 
@@ -32,61 +21,44 @@ class RadioStationsSnapshotsCollector:
     def __init__(self,
                  spotify_client: SpotifyClient,
                  artists_database_inserter: ArtistsDatabaseInserter,
-                 albums_database_inserter: AlbumsDatabaseInserter):
+                 albums_database_inserter: AlbumsDatabaseInserter,
+                 tracks_database_inserter: TracksDatabaseInserter,
+                 audio_features_database_inserter: AudioFeaturesDatabaseInserter):
         self._spotify_client = spotify_client
         self._artists_database_inserter = artists_database_inserter
         self._albums_database_inserter = albums_database_inserter
+        self._tracks_database_inserter = tracks_database_inserter
+        self._audio_features_database_inserter = audio_features_database_inserter
 
     async def collect(self) -> None:
         logger.info('Starting to run `RadioStationsSnapshotsCollector`')
         playlists = await self._spotify_client.playlists.collect(['18cUFeM5Q75ViwevsMQM1j'])
-        stations = await self._collect_stations(playlists)
-        dfs = [station.to_dataframe() for station in stations]
-        data = pd.concat(dfs)
+        await self._insert_records_to_db(playlists)
+        logger.info('Successfully collected and inserted playlists to DB')
 
-        self._export_results(data)
-
-    async def _collect_stations(self, playlists: List[Playlist]) -> List[Station]:
-        stations = []
-
+    async def _insert_records_to_db(self, playlists: List[dict]) -> List[Station]:
         for playlist in playlists:
-            playlist_tracks = await self._get_playlist_tracks(playlist)
-            station = Station(playlist=playlist, tracks=playlist_tracks)
-            stations.append(station)
+            await self._insert_spotify_records(playlist)
+            await self._insert_radio_tracks_records()
 
-        return stations
-
-    async def _get_playlist_tracks(self, playlist: Playlist) -> List[Track]:
-        print(f'Starting to collect `{playlist[ID]}` station artists')
+    async def _insert_spotify_records(self, playlist: dict) -> None:
+        print(f'Starting to insert playlist `{playlist[ID]}` spotify records')
         tracks = playlist[TRACKS][ITEMS]
-        artists = await self._artists_database_inserter.insert(tracks)
-        albums = await self._albums_database_inserter.insert(tracks)
 
-        return self._serialize_tracks(tracks, artists)
+        for database_inserter in self._ordered_database_inserters:
+            await database_inserter.insert(tracks)
 
-    @staticmethod
-    def _serialize_tracks(raw_tracks: List[dict], artists: List[Artist]) -> List[Track]:
-        tracks = []
+    async def _insert_radio_tracks_records(self, playlist: dict):  # TODO: Complete
+        pass
 
-        for raw_track, artist in zip(raw_tracks, artists):
-            inner_track = raw_track.get(TRACK, {})
-
-            if inner_track is not None:
-                track = Track.from_raw_track(inner_track, raw_track, artist)
-                tracks.append(track)
-
-        return tracks
-
-    @staticmethod
-    def _export_results(data: DataFrame) -> None:
-        now = get_current_datetime()
-        output_path = RADIO_STATIONS_PLAYLIST_SNAPSHOT_PATH_FORMAT.format(now)
-        to_csv(data=data, output_path=output_path)
-        file_metadata = GoogleDriveUploadMetadata(
-            local_path=output_path,
-            drive_folder_id=os.environ[RADIO_STATIONS_SNAPSHOTS_DRIVE_ID]
-        )
-        upload_files_to_drive(file_metadata)
+    @property
+    def _ordered_database_inserters(self) -> List[BaseDatabaseInserter]:
+        return [
+            self._artists_database_inserter,
+            self._albums_database_inserter,
+            self._tracks_database_inserter,
+            self._audio_features_database_inserter
+        ]
 
 
 if __name__ == '__main__':
@@ -97,7 +69,9 @@ if __name__ == '__main__':
     snapshots_collector = RadioStationsSnapshotsCollector(
         spotify_client=spotify_client,
         artists_database_inserter=ArtistsDatabaseInserter(db_engine, spotify_client),
-        albums_database_inserter=AlbumsDatabaseInserter(db_engine)
+        albums_database_inserter=AlbumsDatabaseInserter(db_engine),
+        tracks_database_inserter=TracksDatabaseInserter(db_engine),
+        audio_features_database_inserter=AudioFeaturesDatabaseInserter(db_engine, spotify_client)
     )
     loop = asyncio.get_event_loop()
     loop.run_until_complete(snapshots_collector.collect())
