7f486e00b56e5031c51539c9f4d6babf2059165d
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-12-08 22:25:25 +0200

    Replaced embeddings script with script that uploads directly do DB

--- scripts/embeddings_main.py
+++ scripts/embeddings_main.py
@@ -1,16 +1,26 @@
 import asyncio
 
-from data_collection.openai.embeddings.track_names_embeddings_collector import TrackNamesEmbeddingsCollector
+from data_collectors.components import ComponentFactory
+from genie_datastores.milvus import MilvusClient
+
 from tools.environment_manager import EnvironmentManager
 
 
-async def run(chunk_size: int, max_chunks_number: int) -> None:
+async def run(limit: int) -> None:
     EnvironmentManager().set_env_variables()
+    factory = ComponentFactory()
+    milvus_uri = factory.env.get_milvus_uri()
+    openai_api_key = factory.env.get_openai_api_key()
 
-    async with TrackNamesEmbeddingsCollector(chunk_size=chunk_size, chunks_limit=max_chunks_number) as collector:
-        await collector.collect()
+    async with factory.sessions.get_openai_session(openai_api_key) as session:
+        async with MilvusClient(uri=milvus_uri) as client:
+            manager = factory.get_track_names_embeddings_manager(
+                client_session=session,
+                milvus_client=client
+            )
+            await manager.run(limit=limit)
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(chunk_size=50, max_chunks_number=5))
+    loop.run_until_complete(run(limit=100))
