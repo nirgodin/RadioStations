4e41c74d70dd14d1f88f7126a4b645ad6d20f7b5
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-12-22 12:20:48 +0200

    Upgraded wikipedia_age_main.py to use data-collectors

--- scripts/wikipedia_age_main.py
+++ scripts/wikipedia_age_main.py
@@ -1,13 +1,20 @@
 import asyncio
 
-from data_collection.wikipedia.age.wikipedia_link_age_collector import WikipediaAgeLinkCollector
+from data_collectors.components import ComponentFactory
+
+from tools.environment_manager import EnvironmentManager
 
 
 async def run() -> None:
-    collector = WikipediaAgeLinkCollector()
-    await collector.collect()
+    factory = ComponentFactory()
+    link_manager = factory.wikipedia.get_artists_age_link_manager()
+    await link_manager.run(100)
+
+    name_manager = factory.wikipedia.get_artists_age_name_manager()
+    await name_manager.run(100)
 
 
 if __name__ == '__main__':
+    EnvironmentManager().set_env_variables()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
