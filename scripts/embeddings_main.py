6ea479dfebf3fcdd1de31a6b310a36d8b2d6e65f
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-01-08 22:07:26 +0200

    Fix bug in embeddings collection

--- scripts/embeddings_main.py
+++ scripts/embeddings_main.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from data_collectors.components import ComponentFactory
 from genie_datastores.milvus import MilvusClient
@@ -9,11 +10,12 @@ from tools.environment_manager import EnvironmentManager
 async def run(limit: int) -> None:
     EnvironmentManager().set_env_variables()
     factory = ComponentFactory()
-    milvus_uri = factory.env.get_milvus_uri()
     openai_api_key = factory.env.get_openai_api_key()
+    milvus_uri = factory.env.get_milvus_uri()
+    milvus_token = os.environ["MILVUS_TOKEN"]
 
     async with factory.sessions.get_openai_session(openai_api_key) as session:
-        async with MilvusClient(uri=milvus_uri) as client:
+        async with MilvusClient(uri=milvus_uri, token=milvus_token) as client:
             manager = factory.misc.get_track_names_embeddings_manager(
                 client_session=session,
                 milvus_client=client
