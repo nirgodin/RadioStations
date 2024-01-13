efd6bff70e654ec7add1db2470990822a9de8ecc
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-01-13 22:14:37 +0200

    Created glglz chart entries script

---
+++ scripts/glglz_chart_entries.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from data_collectors.components import ComponentFactory
+
+from tools.environment_manager import EnvironmentManager
+
+
+async def run_glglz_charts_manager() -> None:
+    factory = ComponentFactory()
+    session = factory.sessions.get_spotify_session()
+
+    async with session as spotify_session:
+        manager = factory.radio_charts.get_glglz_charts_manager(spotify_session)
+        await manager.run(dates=None, limit=4)
+
+
+if __name__ == '__main__':
+    EnvironmentManager().set_env_variables()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_glglz_charts_manager())
