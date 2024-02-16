33c55f0dec651ac1e1a338db4aee6878321e6bff
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-02-16 13:17:55 +0200

    Created primary genres script

---
+++ scripts/primary_genres_main.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from data_collectors.components import ComponentFactory
+
+from tools.environment_manager import EnvironmentManager
+
+
+async def run_primary_genres_manager() -> None:
+    factory = ComponentFactory()
+    manager = await factory.misc.get_primary_genre_manager()
+
+    await manager.run(limit=500)
+
+
+if __name__ == '__main__':
+    EnvironmentManager().set_env_variables()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_primary_genres_manager())
