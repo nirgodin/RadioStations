e9aa15960b4a927a0adf65e8d0b9252a2a56e702
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-05-16 15:31:42 +0300

    Fix embeddings script

--- scripts/embeddings_main.py
+++ scripts/embeddings_main.py
@@ -1,8 +1,6 @@
 import asyncio
-import os
 
 from data_collectors.components import ComponentFactory
-from genie_datastores.milvus import MilvusClient
 
 from tools.environment_manager import EnvironmentManager
 
@@ -10,17 +8,13 @@ from tools.environment_manager import EnvironmentManager
 async def run(limit: int) -> None:
     EnvironmentManager().set_env_variables()
     factory = ComponentFactory()
-    openai_api_key = factory.env.get_openai_api_key()
-    milvus_uri = factory.env.get_milvus_uri()
-    milvus_token = os.environ["MILVUS_TOKEN"]
 
-    async with factory.sessions.get_openai_session(openai_api_key) as session:
-        async with MilvusClient(uri=milvus_uri, token=milvus_token) as client:
-            manager = factory.misc.get_track_names_embeddings_manager(
-                client_session=session,
-                milvus_client=client
-            )
-            await manager.run(limit=limit)
+    async with factory.tools.get_milvus_client() as milvus_client:
+        retrieval_manager = factory.misc.get_track_names_embeddings_retriever(milvus_client)
+        await retrieval_manager.run()
+
+    collection_manager = factory.misc.get_track_names_embeddings_manager()
+    await collection_manager.run(limit=limit)
 
 
 if __name__ == '__main__':
