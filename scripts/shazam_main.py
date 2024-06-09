261aa439200323a0261e6335e4798cab65a9baa5
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-06-09 22:23:38 +0300

    Remove legacy shazam scripts

--- scripts/shazam_main.py
+++ scripts/shazam_main.py
@@ -19,18 +19,6 @@ async def run_shazam_missing_ids_manager(component_factory: ComponentFactory) ->
 
 async def run():
     EnvironmentManager().set_env_variables()
-
-    cities_fetcher = ShazamCitiesFetcher()
-    print('Starting to fetch data of israeli cities')
-    await cities_fetcher.fetch_cities_top_tracks()
-
-    country_fetcher = ShazamCountryFetcher()
-    print('Starting to fetch data of Israel')
-    await country_fetcher.fetch_country_top_tracks()
-
-    print('Starting to fetch data of world')
-    await country_fetcher.fetch_world_top_tracks()
-
     component_factory = ComponentFactory()
     await run_shazam_top_tracks_manager(component_factory)
     await run_shazam_missing_ids_manager(component_factory)
