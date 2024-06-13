2967bb398ba7145bc04662d58621cd5abec09d22
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-06-13 19:52:50 +0300

    Removed legacy radio stations collection

--- scripts/spotify_main.py
+++ scripts/spotify_main.py
@@ -1,31 +1,22 @@
 import asyncio
 
-from aiohttp import ClientSession
 from data_collectors.components import ComponentFactory
 from genie_datastores.postgres.models import SpotifyStation
 
-from data_collection.spotify.collectors.radio_stations_snapshots.radio_stations_snapshots_collector import \
-    RadioStationsSnapshotsCollector
 from tools.environment_manager import EnvironmentManager
-from utils.spotify_utils import build_spotify_headers
 
 STATIONS = [
     SpotifyStation.GLGLZ,
     SpotifyStation.ECO_99,
     SpotifyStation.FM_103,
     SpotifyStation.KAN_GIMEL,
-    SpotifyStation.KAN_88
+    SpotifyStation.KAN_88,
+    SpotifyStation.KZ_RADIO
 ]
 
 
 async def run() -> None:
     EnvironmentManager().set_env_variables()
-
-    async with ClientSession(headers=build_spotify_headers()) as session:
-        radio_stations_snapshots_collector = RadioStationsSnapshotsCollector(session)
-        await radio_stations_snapshots_collector.collect()
-
-    #########################################################################
 
     component_factory = ComponentFactory()
     spotify_session = component_factory.sessions.get_spotify_session()
