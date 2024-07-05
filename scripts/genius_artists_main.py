c72bc4e4f892078a03f1bc5ec31331e1c4290963
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-07-05 12:50:38 +0300

    Added genius artists script

---
+++ scripts/genius_artists_main.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from data_collectors.components import ComponentFactory
+
+from tools.environment_manager import EnvironmentManager
+
+
+async def run_genius_artists():
+    factory = ComponentFactory()
+    token = factory.env.get_genius_access_token()
+    genius_session = factory.sessions.get_genius_session(token)
+
+    async with genius_session as session:
+        manager = await factory.genius.get_artists_manager(session)
+        await manager.run(200)
+
+
+if __name__ == '__main__':
+    EnvironmentManager().set_env_variables()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_genius_artists())
